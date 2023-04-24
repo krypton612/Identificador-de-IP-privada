39c9db94bb1a9f107816b44127eb98af37661838
Ronald Diaz <50895807+krypton612@users.noreply.github.com>
2023-04-24 08:50:42 -0400

    Update utils.py
    comentarios definidos

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ def ip_list_generator():
     ip_list = []
 
 # Generar direcciones IP de la subred 10.39.144.0 a 10.39.174.255
+# cambiar el valor cuando sea necesario, si tiene en su posibilidad un sistema de mulitprocesadores procure mappear toda la subred apartir
+# del octavo bit para mejores resultados, el mapeo tomara tiempo por lo que vaya y tome una taza de cafe mientras tanto
     for i in range(150, 175):
         for j in range(0, 256):
             ip = "10.39.{}.{}".format(i, j)
